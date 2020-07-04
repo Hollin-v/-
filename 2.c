c048171b21c4592bda7a1a3115d0a5f8650adde0
Hollin-v <56746991+Hollin-v@users.noreply.github.com>
2020-07-04 23:15:06 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,1 +1,45 @@
 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node {
+	int data;
+	struct Node *next;
+}node;
+
+node* createList() {
+	node *head = (node*)malloc(sizeof(node));
+	node *temp = head;
+	head->next = NULL;
+	int data;
+	scanf("%d", &data);
+	while (data)
+	{	
+		node *cur= (node*)malloc(sizeof(node));
+		cur->next = NULL;
+		cur->data = data;
+		temp->next = cur;
+		temp = cur;
+		scanf("%d", &data);
+
+	}
+
+	return head;
+}
+
+void main(int argc, char* argv[]) {
+
+	node *head=createList();
+	node *p = head->next;
+	while (p)
+	{
+		printf("%d\n", p->data);
+		p = p->next;
+	}
+
+	system("pause");
+
+}
